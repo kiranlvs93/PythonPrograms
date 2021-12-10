939da1392479aa4108f7fe86dd81851b6a2fa4c4
k0l05t5 <kiran.lvs@walmart.com>
2021-12-11 01:09:31 +0530

    Checking if a year is leap or not.

---
+++ linkedin_learning/leap_year.py
@@ -0,0 +1,19 @@
+def leap_year_one_line(year):
+    print("Leap year" if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) else "Not a leap year.")
+
+
+def leap_year_if_cond(year):
+    leap = False
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0:
+            leap = False
+        if year % 400 == 0:
+            leap = True
+    print("Leap year" if leap else "Not a leap year")
+
+
+if __name__ == '__main__':
+    year = int(input("Which year do you want to check? "))
+    leap_year_one_line(year)
+    leap_year_if_cond(year)
