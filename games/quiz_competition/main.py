42b1ac61a64afcaa9e49b99ebdd5b7cec45f6469
kiranlvs93 <kiranlvs93@gmail.com>
2021-12-22 01:02:50 +0530

    Adding files for creating a QUIZ competition using OOPs

---
+++ games/quiz_competition/main.py
@@ -0,0 +1,10 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for data in question_data:
+    question_bank.append(Question(data["text"], data["answer"]))
+
+print(len(question_bank))
+print(question_bank[0].text)
+print(question_bank[0].answer)
