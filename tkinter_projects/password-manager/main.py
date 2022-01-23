b54fceca89f0703ebe752b1ceef1c51b60acb42b
kiranlvs93 <kiranlvs93@gmail.com>
2022-01-23 23:08:42 +0530

    Adding basic code for password manager

---
+++ tkinter_projects/password-manager/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("PASSWORD MANAGER")
+window.config(width=400, height=400, padx=100, pady=100)
+
+# Adding canvas
+canvas = Canvas()
+
+window.mainloop()
