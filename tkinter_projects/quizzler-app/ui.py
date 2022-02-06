f52d38bae9cde2fe1c2bb0059d2abd86419e8c10
kiranlvs93 <kiranlvs93@gmail.com>
2022-02-06 23:30:45 +0530

    Reading question from API directly and added UI

--- tkinter_projects/quizzler-app/ui.py
+++ tkinter_projects/quizzler-app/ui.py
@@ -1,2 +1,14 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz App")
+
+        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+
+        self.window.mainloop()
