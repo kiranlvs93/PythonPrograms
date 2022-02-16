ddddfc33b0a70fc03f415d201d0c2eabd53c92b8
kiranlvs93 <kiranlvs93@gmail.com>
2022-02-17 01:39:55 +0530

    Final version of quiz app

--- tkinter_projects/quizzler-app/ui.py
+++ tkinter_projects/quizzler-app/ui.py
@@ -3,6 +3,8 @@ from quiz_brain import *
 
 THEME_COLOR = "#375362"
 WHITE = "#FFFFFF"
+RED = "#EE665D"
+GREEN = "#32CD32"
 FONT_NAME = "Arial"
 
 
@@ -15,11 +17,10 @@ class QuizInterface:
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
         # Configure Score
-        self.score = 0
-        self.score_label = Label(text=f"Score:{self.score}", bg=THEME_COLOR, fg=WHITE, font=(FONT_NAME, 10, "bold"))
+        self.score_label = Label(text=f"Score:0", bg=THEME_COLOR, fg=WHITE, font=(FONT_NAME, 10, "bold"))
         self.score_label.grid(row=0, column=1, sticky="E")
 
-        # Configure entry box
+        # Configure question display
         self.canvas = Canvas(width=300, height=250, bg=WHITE)
         self.question_text = self.canvas.create_text(150, 125, text="Question??", fill=THEME_COLOR, width=280,
                                                      font=(FONT_NAME, 20, "italic"))
@@ -27,12 +28,12 @@ class QuizInterface:
 
         # true button
         true_image = PhotoImage(file="images/true.png")
-        self.correct_btn = Button(text="Correct", image=true_image)
-        self.correct_btn.grid(row=2, column=0, sticky="W")
+        self.true_btn = Button(text="Correct", image=true_image, command=self.true_button_clicked)
+        self.true_btn.grid(row=2, column=0, sticky="W")
 
         # false button
         false_image = PhotoImage(file="images/false.png")
-        self.false_btn = Button(text="False", image=false_image)
+        self.false_btn = Button(text="False", image=false_image, command=self.false_button_clicked)
         self.false_btn.grid(row=2, column=1, sticky="E")
 
         self.next_question()
@@ -40,5 +41,26 @@ class QuizInterface:
         self.window.mainloop()
 
     def next_question(self):
-        question = self.quiz_ob.next_question()
-        self.canvas.itemconfig(self.question_text, text=question)
+        self.canvas.config(bg=WHITE)
+        if self.quiz_ob.still_has_questions():
+            question = self.quiz_ob.next_question()
+            self.canvas.itemconfig(self.question_text, text=question)
+            self.score_label.configure(text=f"Score:{self.quiz_ob.score}")
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have reached the end of the QUIZ")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+
+
+    def true_button_clicked(self):
+        self.give_feedback(self.quiz_ob.check_answer("true"))
+
+    def false_button_clicked(self):
+        self.give_feedback(self.quiz_ob.check_answer("false"))
+
+    def give_feedback(self, result):
+        if result:
+            self.canvas.configure(bg=GREEN)
+        else:
+            self.canvas.configure(bg=RED)
+        self.window.after(1000, self.next_question)
