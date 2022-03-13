f2cb722353b5112df8a0b2b8e0eb077802e65396
kiranlvs93 <kiranlvs93@gmail.com>
2022-03-13 23:35:35 +0530

    Moved the instance variables to the global variables to follow better coding standards

--- hundred_days_of_code/flight_deals_finder/notification_manager.py
+++ hundred_days_of_code/flight_deals_finder/notification_manager.py
@@ -1,25 +1,20 @@
 from twilio.rest import Client
 import os
 
+TWILIO_ACC_ID = os.getenv("TWILIO_ACC_ID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_MESSAGING_SERVICE_SID = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
+TO_NUMBER = os.getenv("TO_NUMBER")
+
 
 class NotificationManager:
-    TWILIO_ACC_ID = ""
-    TWILIO_AUTH_TOKEN = ""
-    TWILIO_MESSAGING_SERVICE_SID = ""
-    TO_NUMBER = ""
-
     def __init__(self):
-        self.TWILIO_ACC_ID = os.getenv("TWILIO_ACC_ID")
-        self.TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-        self.TWILIO_MESSAGING_SERVICE_SID = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
-        self.TO_NUMBER = os.getenv("TO_NUMBER")
+        self.client = Client(TWILIO_ACC_ID, TWILIO_AUTH_TOKEN)
 
     def send_sms(self, message):
-        client = Client(self.TWILIO_ACC_ID, self.TWILIO_AUTH_TOKEN)
-
-        message = client.messages.create(
-            messaging_service_sid=self.TWILIO_MESSAGING_SERVICE_SID,
+        message = self.client.messages.create(
+            messaging_service_sid=TWILIO_MESSAGING_SERVICE_SID,
             body=message,
-            to=self.TO_NUMBER
+            to=TO_NUMBER
         )
-        print(message.sid)
+        print("Message is sent", message.sid)
