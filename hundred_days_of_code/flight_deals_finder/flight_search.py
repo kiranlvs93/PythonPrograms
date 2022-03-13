057dcf221d1dba2d3eef58cb9418229cfc14f586
kiranlvs93 <kiranlvs93@gmail.com>
2022-03-13 23:44:39 +0530

    Code to find flight and updated the URL with end points and params

--- hundred_days_of_code/flight_deals_finder/flight_search.py
+++ hundred_days_of_code/flight_deals_finder/flight_search.py
@@ -1,15 +1,10 @@
 import os
 import requests
-from datetime import datetime as dt, timedelta
+from flight_data import FlightData
 
-LOCATION_SEARCH_URL = "https://tequila-api.kiwi.com/locations/query?locale=en-US&location_types=city&limit=10" \
-                      "&active_only=true&term={CITY}"
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 FLIGHT_SEARCH_API_KEY = os.getenv("FF_TEQUILA_API")
 HEADERS = {"apikey": FLIGHT_SEARCH_API_KEY}
-
-today = dt.today().date()
-from_date = today + timedelta(days=1)
-to_date = today + timedelta(weeks=6 * 4)
 
 
 class FlightSearch:
@@ -18,19 +13,53 @@ class FlightSearch:
     """
 
     def get_iata_code(self, city_name):
-        response = requests.get(url=LOCATION_SEARCH_URL.format(CITY=city_name), headers=HEADERS)
+        location_search_url = f"{TEQUILA_ENDPOINT}/locations/query"
+        query = {"locale": "en-US", "location_types": "city", "limit": "10",
+                 "active_only": "true", "term": city_name}
+        response = requests.get(url=location_search_url, headers=HEADERS, params=query)
         response.raise_for_status()
-        response = response.json()
-        for location in response["locations"]:
+        response = response.json()["locations"]
+        for location in response:
             if location["name"].lower() == city_name.lower():
                 print(f"Found IATA code for {city_name}:: {location['code']}")
                 return location['code']
 
-    def find_flight(self, from_city_code, to_city_code):
-        """
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "INR",
+            "sort": "price",#duration, quality,date
+            "asc": 1
+        }
 
-        :param from_city_code:
-        :param to_city_code:
-        :return:
-        """
-        pass
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=HEADERS,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: Rs.{flight_data.price}")
+        return flight_data
