287d765a82b7bada5f67a0e2c4dfc383331900da
kiranlvs93 <kiranlvs93@gmail.com>
2022-03-13 15:06:35 +0530

    Code to update the missing IATA code

--- hundred_days_of_code/flight_deals_finder/main.py
+++ hundred_days_of_code/flight_deals_finder/main.py
@@ -1,22 +1,36 @@
-# This file will need to use the DataManager,FlightSearch, FlightData,
-# NotificationManager classes to achieve the program requirements.
+import json
 import os
 from notification_manager import NotificationManager
-import data_manager
 from data_manager import DataManager
 from flight_data import FlightData
+from flight_search import FlightSearch
 
 SHEET_NAME = os.getenv("FF_SHEET_NAME")
 data_mng = DataManager(SHEET_NAME)
+flight_search = FlightSearch()
 #
 # notifications = NotificationManager()
 # notifications.send_sms("Test SMS")
 
+PUT_BODY = {"price": {}}
 
-def read_sheet():
-    print(data_mng.read_sheet())
+
+def update_iata_code(input_sheet):
+    for row in input_sheet:
+        city = row["city"]
+        iata_code = row["iataCode"]
+        lowest_price = row["lowestPrice"]
+        row_no = row["id"]
+        print(city, iata_code, lowest_price, row_no, sep=",")
+        if iata_code == "":
+            row["iataCode"] = flight_search.get_iata_code(city_name=city)
+            PUT_BODY["price"] = row
+            data_mng.update_row(params=PUT_BODY, row_num=row_no)
+    print("IATA code for all the cities is updated successfully")
 
 
 if __name__ == '__main__':
-    # read_sheet()
-    print(data_mng.sheet_data)
+
+    # Update the location IATA code if its empty
+    prices_sheet = data_mng.sheet_data["prices"]
+    update_iata_code(input_sheet=prices_sheet)
